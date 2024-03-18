241ce93ff05298b7a3101e3650f3a206cc27fa73
Tejaswi Ganti <tejaswi.gln@gmail.com>
2024-03-18 12:17:56 +0530

    Including Hyper param tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,15 +18,25 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
     except Exception as e:
-        raise   CustomException(e, sys)          
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+        raise   CustomException(e, sys)   
+    
+           
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
 
-            model.fit(X_train, y_train) # Train model
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=10)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train, y_train) # Train model
 
             y_train_pred =model.predict(X_train)
 
